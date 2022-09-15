34ec516c31e985ad658e88ca6121e3610be78267
DIASENGUPTA <50639028+DIASENGUPTA@users.noreply.github.com>
2022-09-15 13:20:49 -0400

    Update tree

--- labs/01-lab/infrastructure/tree.py
+++ labs/01-lab/infrastructure/tree.py
@@ -1,3 +1,21 @@
 def draw_tree():
-    print("tree not found")
-    return
+    k = 2 * n - 2   
+    for i in range(0, n):    
+        for j in range(0, k):  
+            print(end=" ")  
+        k = k - 1   
+        for j in range(0, i + 1):  
+            print("- ", end="")  
+        print("")  
+    k = n - 2  
+    for i in range(n, -1, -1):  
+        for j in range(k, 0, -1):  
+            print(end=" ")   
+        k = k + 1  
+        for j in range(0, i + 1):  
+            print("- ", end="")  
+        print("")  
+    for i in range(0,n):
+        print(" "*12, end="")
+        print("|"*4)
+ draw_tree(8)
